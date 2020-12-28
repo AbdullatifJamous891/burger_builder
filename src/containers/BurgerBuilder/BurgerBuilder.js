1604f91ed236b33e333862a95f60fc27229f68e3
abdullatifJamous891 <abdullatif.jamous891@gmail.com>
2020-12-28 06:37:03 +0200

    backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,6 +78,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false})
     }
 
+    purchaseContinueHandler = () => {
+        alert('Your purchase continued...')
+    }
+
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -88,7 +92,11 @@ class BurgerBuilder extends Component {
         return(
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummery ingredients={this.state.ingredients} />
+                    <OrderSummery 
+                        ingredients={this.state.ingredients}
+                        price={this.state.totalPrice.toFixed(2)}
+                        purchaseCanceled={this.purchaseCancelHandler}
+                        purchasedContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}>Burger</Burger>
                 <BuildControls 
